49c461d7ee80832552ff8cf10587e1a3296649bd
Noumansharif27 <noumansharifgul27@gmail.com>
2026-07-14 11:57:19 -0400

    decideing the required btns for sidepanel

--- SocialMedia/src/components/Button.jsx
+++ SocialMedia/src/components/Button.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-function Button({ content }) {
+function Button({ content, width, height, context }) {
   return (
-    <button className="h-12 flex justify-center items-center bg-blue-700 w-12 rounded-full overflow-hidden">
+    <button
+      style={{ width: width ? `${width}px` : "auto" },{height : height ? `${height}px`: "48px"}}
+      className="flex flex-col justify-center items-center bg-blue-700 w-12 rounded-full overflow-hidden bg-gray-200"
+    >
       {content}
+      {context}
     </button>
   );
 }
--- SocialMedia/src/components/Sidebar.jsx
+++ SocialMedia/src/components/Sidebar.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import Button from "./Button";
+import { Plus, House } from "lucide-react";
 
 function Sidebar() {
   return (
     <>
-      <div className="col-span-2 row-span-10 bg-green-500 p-2">SideBar</div>
+      <div className="flex flex-col gap-5 col-span-2 row-span-10 px-15 py-8">
+        <Button
+          content={<Plus size={20} />}
+          width={90}
+          height={80}
+          context={"New"}
+        />
+        <p className="flex gap-2">
+          <House size={20} />
+          Home
+        </p>
+      </div>
     </>
   );
 }
