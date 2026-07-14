8cf11fbd011c30ff1bb708e82e4c77b6b7bc3de9
Noumansharif27 <noumansharifgul27@gmail.com>
2026-07-14 11:34:19 -0400

    stsrting on the SideBar alignments and addion of elements

--- SocialMedia/src/components/Navbar.jsx
+++ SocialMedia/src/components/Navbar.jsx
@@ -2,13 +2,16 @@ import React, { Component } from "react";
 import Button from "./Button";
 import { Settings, UserRound, Info, Search } from "lucide-react";
 import profileImage from "../assets/profileImage.jpg";
+import logo from "../assets/logo.png";
 
 function Navbar() {
   return (
     <>
-      <div className="col-span-10 row-span-1 bg-blue-400 flex justify-between items-center p-4">
-        <div className="left flex-1">logo</div>
-        <div className="flex flex-2 bg-purple-200 gap-4 items-center py-2 px-4 rounded-full w-100 bg-gray-200">
+      <div className="col-span-10 row-span-1 flex justify-between items-center p-4">
+        <div className="left flex-1 h-22 max-w-22 mr-60 ml-30 rounded-full bg-black">
+          <img src={logo} alt="logo" className="object-contain max-h-full" />
+        </div>
+        <div className="flex flex-2 gap-4 items-center py-2 px-4 rounded-full w-100 bg-gray-200">
           <Search size={20} className="text-left" />
           <input type="text" placeholder="Search items" />
         </div>
